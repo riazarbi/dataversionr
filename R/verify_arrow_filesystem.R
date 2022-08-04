3e9c08dd96aca56ddb6ea68676c9130deb9d8aa8
riaz arbi <riazarbi@gmail.com>
2022-08-04 11:20:27 +0200

    Remove functions moved to diffdfs package

--- R/verify_arrow_filesystem.R
+++
@@ -1,8 +0,0 @@
-
-verify_arrow_filesystem <- function(destination, verbose = FALSE) {
-  if(!(all(class(destination) == c("SubTreeFileSystem","FileSystem","ArrowObject","R6")))) {
-    stop("destination parameter is not an arrow filesystem object.")
-  } else if (verbose) {
-    message("destination parameter is an arrow filesystem object.")
-  }
-}
