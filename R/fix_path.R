f6d411f0370138630b6749fff82d124fe6685103
riaz arbi <riazarbi@gmail.com>
2022-08-04 11:23:10 +0200

    Add fix_path helper function

---
+++ R/fix_path.R
@@ -0,0 +1,20 @@
+fix_path <- function(path,
+                     destination,
+                     verbose = FALSE) {
+
+    destination <- make_SubTreeFileSystem(destination, verbose = verbose)
+
+    destination$base_path
+
+    base_fs <- destination$base_fs
+    base_path <- destination$base_path
+    put_path <- file.path(base_path, path, fsep = )
+    put_path <- gsub("//", "/", put_path)
+
+    if("LocalFileSystem" %in% class(destination$base_fs)) {
+    put_location <- put_path
+  } else {
+    put_location <- base_fs$path(put_path)
+  }
+  return(put_location)
+}
