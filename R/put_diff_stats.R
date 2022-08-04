06e4ab14120212fa4691d5dea64d39be243576cc
riaz arbi <riazarbi@gmail.com>
2022-08-04 11:22:31 +0200

    add missing put_diff_stats

---
+++ R/put_diff_stats.R
@@ -0,0 +1,11 @@
+put_diff_stats <- function(destination) {
+
+  destination <- make_SubTreeFileSystem(destination)
+
+  diff_counts_df <- summarise_diff(destination)
+
+  put_location <- fix_path("diff_stats.csv", destination)
+
+  write_csv_arrow(diff_counts_df, put_location)
+  return(TRUE)
+}
